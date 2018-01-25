19c36c0536f927185ba4b19a6beca4495101cac4
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-01-25 18:42:24 +0100

    rangement + rectification lien

--- view/Forum.php
+++ view/Forum.php
@@ -5,15 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="logo faidherbe.jpg">
+    <link rel="icon" href="C:/wamp64/www/Site-Faidherbe/ressource/logo faidherbe.jpg">
 
     <title>Page Forum </title>
 
     <!-- Bootstrap core CSS -->
-    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="Accueil.css" rel="stylesheet">
+    <link href="css/Accueil.css" rel="stylesheet">
   </head>
 
   <body>
@@ -63,8 +63,8 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../../../dist/js/bootstrap.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
