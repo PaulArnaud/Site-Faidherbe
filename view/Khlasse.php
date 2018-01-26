d01b54f23d7e6291d090f7f375f58ee91b9d2487
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-01-26 18:21:32 +0100

    navbar en plus et require

--- view/Khlasse.php
+++ view/Khlasse.php
@@ -18,36 +18,7 @@
 
   <body>
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a>
-		<img src="stickers-la-goudale.jpg" width="50" height="50" alt="">
-	</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="Accueil.php">  Accueil </a>
-          </li>
-          <li class="nav-item active">
-			<a class="nav-link" href="Khlasse.php"> Khlâsse <span class="sr-only">(current)</span>  </a>
-          </li>
-		  <li class="nav-item">
-            <a class="nav-link" href="Mon Profil.php"> Mon profil </a>
-          </li>
-		  <li class="nav-item">
-            <a class="nav-link" href="Forum.php"> Forum </a>
-          </li>
-        </ul>
-		<ul class="navbar-nav px-3">
-			<li class="nav-item text-nowrap">
-			  <a class="nav-link" href="#"> Se déconnecter </a>
-			</li>
-		</ul>
-      </div>
-    </nav>
+  <?php require('navbar.php') ?>
 
     <main role="main" class="container">
 		<div class="starter-template">
