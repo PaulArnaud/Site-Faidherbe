2b03780bab11bbebe8e35e72ee571dcd98214290
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-21 17:37:58 +0200

    aa

--- inscription.php
+++ inscription.php
@@ -12,24 +12,26 @@
         <form "form-signin" >
             <img class="mb-4" src="logo faidherbe.jpg" alt="" width="150" height="150">
             
-            <div class="form-group">
+            <div class="input-group">
                 <label for="Nom"></label>
                 <input type="nom" class="form-control-lg" id="inputnom" placeholder="Votre nom" required >
             </div>
-            <div class="form-group">
+            <div class="input-group">
                 <label for="Prenom"></label>
                 <input type="prenom" class="form-control-lg" id="inputprenom" placeholder="Votre prénom" required >
             </div>
-            <div class="form-group">
+            <div class="input-group">
                 <label for="Email1"></label>
-                <span class="input-group-text">@</span>
+                <div class="input-group-prepend">
+                  <span class="input-group-text">@</span>
+                </div>
                 <input type="email" class="form-control-lg" id="inputemail" placeholder="Votre mail" required>
             </div>
-            <div class="form-group">
+            <div class="input-group">
                 <label for="Motivation"></label>
                 <textarea class="form-control-lg" id="zonetexte"  placeholder ="Expliquez nous vos motivations en quelques mots" required ></textarea>
             </div>
-            </div>
+            
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </form>
 
