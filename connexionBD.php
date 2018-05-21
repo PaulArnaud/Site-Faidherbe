09638e8d4e4f78c118793c0884cd7a44321202ae
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-21 14:32:59 +0200

    Connexion

---
+++ connexionBD.php
@@ -0,0 +1,17 @@
+<?php
+function myPDO() {
+    // Base de données Heroku Décibels
+    $dsn = "ec2-54-228-181-43.eu-west-1.compute.amazonaws.com;dbname=d84nlnmfgu4vji";
+    $login = "csgwojauygcbjo";
+    $mdp = "d0fe7c66c47196fea644214cf2cd232a65f0a805ce28d3d798115e11df7b68b0";
+    // Création de la connexion
+    try {
+        $bd = new PDO($dsn, $login, $mdp);
+        $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (Exception $e) {
+        echo 'Error creating PDO -> ';
+        var_dump($e->getMessage());
+    }
+    return $bd;
+}
