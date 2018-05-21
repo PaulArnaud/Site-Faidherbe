162b8b872609e39a1543f5dd21a4073366b6ac09
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-21 17:25:04 +0200

    button

--- connexion.php
+++ connexion.php
@@ -12,10 +12,15 @@
 		<form class="form-signin">
 			<img class="mb-4" src="logo faidherbe.jpg" alt="" width="150" height="150">
 			<h1 class="h3 mb-3 font-weight-normal"> Bienvenue </h1>
+
 			<label for="inputEmail" class="sr-only"> Mail </label>
+			<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
 			<input type="email" id="inputEmail" class="form-control" placeholder=" Mail" required autofocus>
+	
 			<label for="inputPassword" class="sr-only"> Mot de Passe </label>
+			<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
 			<input type="password" id="inputPassword" class="form-control" placeholder="Mot de Passe" required>
+			
 			<button class="btn btn-lg btn-block" type="submit"> Se connecter </button>
 			<a class="btn btn-lg btn-block" type="submit" href="inscription.php"> S'inscrire </a>
 			<p class="mt-5 mb-3 text-muted">
