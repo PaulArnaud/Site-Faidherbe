b2df03dd15fba511e2b683b57b3782242e644cdc
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-23 20:47:41 +0200

    aa

--- webroot/index.php
+++ webroot/index.php
@@ -1,8 +1,10 @@
 <?php 
 define('WEBROOT',dirname(__FILE__));
+define('BROOT',dirname(WEBROOT));
+define('DS',DIRECTORY_SEPARATOR);
+define('CORE',ROOT.DS.'core');
+define('BASE_URL',dirname(dirname($_SERVER['SCRIPT_NAME'])));
+
+echo BASE_URL;
 ?>
 
-<pre>
-    <?php print_r($_SERVER);
-    echo __FILE__ ; ?>
-</pre>
