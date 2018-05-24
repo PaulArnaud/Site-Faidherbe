0287f0ff990645fedba93d6a665d1b477675a55b
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-24 16:52:54 +0200

    a
    a

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
 <meta charset="utf-8">
-<link rel="stylesheet" href="/Site_Faidherbe/lib/bootstrap.min.css">
-<script src="/Site_Faidherbe/lib/angular.min.js"></script>
+<link rel="stylesheet" href="lib/bootstrap.min.css">
+<script src="lib/angular.min.js"></script>
