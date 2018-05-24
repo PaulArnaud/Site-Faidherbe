04e0f763ae781f7c832fe190070ef0a826ab31d6
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-24 16:44:14 +0200

    rangement

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
 <meta charset="utf-8">
-<link rel="stylesheet" href="bootstrap.min.css">
-<script src="angular.min.js"></script>
+<link rel="stylesheet" href="/Site-Faidherbe/lib/bootstrap.min.css">
+<script src="/Site-Faidherbe/lib/angular.min.js"></script>
