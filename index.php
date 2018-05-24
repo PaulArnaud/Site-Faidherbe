6d19f229f1610df292f40c9e54913229e6786cf3
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-24 17:59:39 +0200

    aaaa

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-$url = '';
-if (isset($_GET['url'])){
-    $url = explode('/',$_GET['url']);
-}
-if ($url == ''){
-    require('/view/pageprincipale/main.php');
-}
-else if ($url[0] == 'chants'){
-    require('/view/pagechants/chants.php');
-}
-else {
-    require('/view/pageprincipale/main.php');
-}
+require('/view/pageprincipale/main.php');
 ?>
