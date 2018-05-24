6981336d9233c2005c5544b7255e2ac0fe1539bd
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-24 17:57:15 +0200

    absolution

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ if (isset($_GET['url'])){
     $url = explode('/',$_GET['url']);
 }
 if ($url == ''){
-    require('view/pageprincipale/main.php');
+    require('/view/pageprincipale/main.php');
 }
 else if ($url[0] == 'chants'){
-    require('view/pagechants/chants.php');
+    require('/view/pagechants/chants.php');
 }
 else {
-    require('view/pageprincipale/main.php');
+    require('/view/pageprincipale/main.php');
 }
 ?>
--- view/communs/header.php
+++ view/communs/header.php
@@ -1,3 +1,3 @@
 <meta charset="utf-8">
-<link rel="stylesheet" href="/Site-faidherbe/lib/bootstrap.min.css">
-<script src="/Site-faidherbe/lib/angular.min.js"></script>
+<link rel="stylesheet" href="/lib/bootstrap.min.css">
+<script src="/lib/angular.min.js"></script>
