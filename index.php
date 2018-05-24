1b792ea098cf7cf9890ecc206374c5307e5c911a
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-24 17:08:13 +0200

    aaesssai

--- index.php
+++ index.php
@@ -5,5 +5,13 @@ $url = '';
 if (isset($_GET['url'])){
     $url = explode('/',$_GET['url']);
 }
-var_dump($url);
+if ($url == ''){
+    require('main.php');
+}
+else if ($url[0] == 'chants'){
+    require('chants.php');
+}
+else {
+    require('accueil.php');
+}
 ?>
