82686c669c4ddc675b4082785c0145e22080c921
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-25 19:21:52 +0200

    aa

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
     define("WEBROOT",dirname(__FILE__));
+    define("ROOT",dirname(WEBROOT));
+    define("DS",DIRECTORY_SEPARATOR);
+    define("CORE",ROOT.DS."core");
     echo WEBROOT;
+    echo ROOT;
+    echo DS;
+    echo CORE;
 ?>
