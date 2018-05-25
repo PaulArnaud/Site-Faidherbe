5e85e63ef18a55a2d59a711dd2f54cb3cc0df66e
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-25 18:25:43 +0200

    zae

--- index.php
+++ index.php
@@ -7,5 +7,4 @@
         <a href="/view/pageprincipale/main.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Site complet</a>
 	</body>
     <?php require('/view/communs/footer.php') ?>
-    <?php echo ça marche pas  ?>
 </html>
