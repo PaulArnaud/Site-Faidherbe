a6aa0cf5295af7f2f983de4d84638747a301f234
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-26 15:39:52 +0200

    encore tutuo

--- core/Routeur.php
+++ core/Routeur.php
@@ -1,6 +1,7 @@
 <?php
 class Routeur{
     static function parse($url){
+        $url = trim($url,'/');
         $params = explode('/',$url);
         print_r($params);
     }
--- webroot/index.php
+++ webroot/index.php
@@ -2,9 +2,9 @@
     define("WEBROOT",dirname(__FILE__));
     define("ROOT",dirname(WEBROOT));
     define("DS",DIRECTORY_SEPARATOR);
-    define("CORE",WEBROOT.DS."core");
+    define("CORE",ROOT.DS."core");
     define("BASE_URL",dirname(dirname($_SERVER["SCRIPT_NAME"])));
-    
+    echo $_SERVER['SERVER_NAME'];
     require(CORE.DS.'includes.php');
     new Dispatcher();
 ?>
