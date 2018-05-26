267d727df93464bd3e8b34b1111ee8fb73dad63e
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-26 22:22:40 +0200

    aze

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     define("DS",DIRECTORY_SEPARATOR);
     define("CORE",ROOT.DS."core");
     define("BASE_URL",dirname(dirname($_SERVER["SCRIPT_NAME"])));
-    require(CORE.DS.'includes.php');
-    echo ' ici iciciciccicicici ';
-    //new Dispatcher();
+    require('/core/includes.php');
+    new Dispatcher();
 ?>
