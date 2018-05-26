8a5e77d6be14f7c53c6097f3d491df6c49b41f6c
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-26 23:00:58 +0200

    aze

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     define("DS",DIRECTORY_SEPARATOR);
     define("CORE",ROOT.DS."core");
     define("BASE_URL",dirname(dirname($_SERVER["SCRIPT_NAME"])));
-    require(CORE.DS.'includes.php');
+    /*require(CORE.DS.'includes.php');*/
     require(ROOT.DS.'config'.DS.'connect.php');
     new Dispatcher();
 ?>
