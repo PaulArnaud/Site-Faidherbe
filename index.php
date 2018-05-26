05fc9c537bfb6a718cba112de461189cc280e0f4
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-26 23:20:45 +0200

    aze

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
     define("BASE_URL",dirname(dirname($_SERVER["SCRIPT_NAME"])));
     /*require(CORE.DS.'includes.php');*/
     require_once('/config/connect.php');
-    echo myPDO()->query('SELECT * FROM khlasse');
+    $res = myPDO()->query('SELECT * FROM khlasse');
     //new Dispatcher();
 ?>
