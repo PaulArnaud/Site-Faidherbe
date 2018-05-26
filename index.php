ebae9807d5082466e1dd841dcbdb92d8dea267f2
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-26 22:18:23 +0200

    aze

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
     define("CORE",ROOT.DS."core");
     define("BASE_URL",dirname(dirname($_SERVER["SCRIPT_NAME"])));
     require(CORE.DS.'includes.php');
-    new Dispatcher();
+    echo ' ici iciciciccicicici ';
+    //new Dispatcher();
 ?>
