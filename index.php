6ff7d6f346323440362427eb1214a5e90fc22983
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-26 22:28:24 +0200

    a

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    define("ROOT",dirname(__FILE__));
+    /*define("ROOT",dirname(__FILE__));
     define("DS",DIRECTORY_SEPARATOR);
     define("CORE",ROOT.DS."core");
     define("BASE_URL",dirname(dirname($_SERVER["SCRIPT_NAME"])));
     require('/core/includes.php');
-    echo "eorkzeokf";
-    new Dispatcher();
+    new Dispatcher();*/
+    require('/app/view/pageconnexion/connexion.php');
 ?>
