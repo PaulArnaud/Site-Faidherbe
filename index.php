a171b205bfb0b5f29d3e8dd1f00c9ab3744bdf81
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-26 22:29:34 +0200

    aze

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-    /*define("ROOT",dirname(__FILE__));
+    define("ROOT",dirname(__FILE__));
     define("DS",DIRECTORY_SEPARATOR);
     define("CORE",ROOT.DS."core");
     define("BASE_URL",dirname(dirname($_SERVER["SCRIPT_NAME"])));
     require('/core/includes.php');
-    new Dispatcher();*/
-    require('/app/view/pageconnexion/connexion.php');
+    new Dispatcher();
 ?>
