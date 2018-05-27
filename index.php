532e0cde111a51151848672cc2c8114d30e69b98
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 19:38:27 +0200

    azeazeaze

--- index.php
+++ index.php
@@ -27,7 +27,8 @@
 				&copy; Faidherbard
 			</p>
 		</form>
-
+		<div>
+		<h1> resultat</h1>
 		<?php
 		require('controller/controller_khlasse.php');
 		$control = new khlasseController();
@@ -37,6 +38,8 @@
 			echo $value['id_khlasse'];
 		}
 		?>
+
+		</div>
 
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
