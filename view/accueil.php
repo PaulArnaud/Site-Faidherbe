59fd3f6d843b8c376787d1a49fb8ee50776899f1
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 22:27:16 +0200

    azeazeaze

--- view/accueil.php
+++ view/accueil.php
@@ -10,21 +10,21 @@
 		<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top site-header">
 			<div class="collapse navbar-collapse" id="navbarsExampleDefault" >
 				<ul class="navbar-nav mr-auto">
-					<li class="nav-item"><a href="Accueil.php">Accueil</a></li>
-					<li class="nav-item"><a href="Deposit.php">Désposer une annonce</a></li>
-					<li class="nav-item"><a href="Consult.php">Consulter les annonces</a></li>
+					<li class="nav-item"><a class="nav-link"  href="Accueil.php">Accueil</a></li>
+					<li class="nav-item"><a class="nav-link"  href="Deposit.php">Désposer une annonce</a></li>
+					<li class="nav-item"><a  class="nav-link" href="Consult.php">Consulter les annonces</a></li>
 					<?php if(isLogged()){ ?>
-					<li class="nav-item"><a href="Personnal_orders.php">Mes commandes</a></li>
+					<li class="nav-item"><a class="nav-link"  href="Personnal_orders.php">Mes commandes</a></li>
 					<?php } ?>
 					<?php if(isAdmin()){ ?>
-					<li class="nav-item"><a href="Administrateur.php">Administrateur</a></li>
+					<li class="nav-item"><a class="nav-link"  href="Administrateur.php">Administrateur</a></li>
 					<?php } ?>
 					<?php if(!isLogged()){ ?>
-					<li class="nav-item"><a href="Connexion.php">Connexion</a></li>
+					<li class="nav-item"><a class="nav-link" href="Connexion.php">Connexion</a></li>
 					<?php } ?>
 					<?php if(isLogged()){ ?>
 					<li class="nav-item">
-						<a title="Déconnexion" href="controller/Controller_Deconnexion.php"><i class="large material-icons">power_settings_new</i></a>
+						<a class="nav-link"  title="Déconnexion" href="controller/Controller_Deconnexion.php"><i class="large material-icons">Deconnexion</i></a>
 					</li>
 					<?php } ?>
 				</ul>
