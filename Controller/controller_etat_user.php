74f6360df120b86ca820012335859e1058bfb7e1
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 21:23:08 +0200

    azeazeaze

--- Controller/controller_etat_user.php
+++ Controller/controller_etat_user.php
@@ -1,35 +1,42 @@
 <?php
 require_once("model/Users.php");
+
 function isLogged(){
   if(isset($_COOKIE['cookieperso'])){
     $cookie=$_COOKIE['cookieperso'];
     $user=Users::Get_User_Id($cookie);
+
     return(!empty($user));
   }
   else return false;
 }
+
 function loggedOnly(){
   if(!isLogged()){
     header("Location: ../Connexion.php");
   }
 }
+
 function unloggedOnly(){
   if(isLogged()){
     header("Location: ../Accueil.php");
   }
 }
+
 function isAdmin(){
   if(isset($_COOKIE['cookieperso'])){
     $cookie=$_COOKIE['cookieperso'];
     $userid=Users::Get_User_Id($cookie);
     $role=Users::Get_Users_Role($userid);
-    if($role===1){
+
+    if($role===true){
       return true;
     }
     else return false;
   }
   else return false;
 }
+
 function adminOnly(){
   if(!isAdmin())
   {
