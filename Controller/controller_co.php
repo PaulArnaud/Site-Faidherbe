36cfab847f2f9123175717214183b640a9f8323b
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 22:52:02 +0200

    azeazeazeaze

---
+++ Controller/controller_co.php
@@ -0,0 +1,24 @@
+
+<?php
+require_once("../model/Users.php");
+$email=htmlspecialchars($_POST['inputEmail']);
+$password=htmlspecialchars($_POST['password']);
+if(empty($email) || empty($password)){
+  $message="Merci de remplir tous les champs!";
+  //header("Location: ../Erreur.php?=".$message);
+}
+else
+{
+  $password=sha1(sha1($password));
+  if(Users::Check_Password($email,$password)){
+    $cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
+		setcookie("cookieperso", $cookie, time()+(60*60*24*30), "/");
+		Users::Set_User_Cookie($email,$cookie);
+		//header("Location: ../Accueil.php");
+  }
+  else {
+    $message="Mauvais mdp";
+    //header("Location: ../Erreur.php?=".$message);
+  }
+}
+?>
