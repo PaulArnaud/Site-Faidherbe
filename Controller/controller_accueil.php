3f5e28c3bbc73071033bec591afc34898ce7a4b4
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 21:50:50 +0200

    azeazeaze

--- Controller/controller_accueil.php
+++ Controller/controller_accueil.php
@@ -1,7 +1,9 @@
-
+<html>
+<h1>ehehehhehe</h1>
+</html>
 
 <?php
 //require_once("model/Users.php");
 //require_once("controller/controller_etat_user.php");
-require_once("view/accueil.php");
+//require_once("view/accueil.php");
 ?>
