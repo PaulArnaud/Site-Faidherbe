e2c80d170685271e21aa208b77c96a9f2555a485
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 11:21:12 +0200

    azeazeaz

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
 		<div class ="container">
 		<?php require('/config/connect.php');
 		$result = myPDO() -> query('SELECT id_khlasse,id_type,annee FROM khlasse');
+		echo 'prout 123 ';
 		print_r(myPDO());
 		$data = $result->fetchAll(PDO::FETCH_ASSOC);
     	foreach ($data as $value) {
