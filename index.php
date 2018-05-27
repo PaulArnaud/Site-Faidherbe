19a9d71308c685ed73cc0fe323ea6b49abdcec28
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 19:07:29 +0200

    546

--- index.php
+++ index.php
@@ -28,13 +28,41 @@
 			</p>
 		</form>
 
-		<div class ="container"> 
-			<?php 
-			require('config/connect.php');
-			$req = myPDO();
-			$data = $req -> query('SELECT * FROM khlasse');
-			print_r($data);
-			?>
+		<div>
+		
+		<?php 
+		//print_r($_SERVER);
+		require('config/connect.php');
+		try
+		{
+		// On se connecte à MySQL
+		$bdd = myPDO();
+		}
+		catch(Exception $e)
+		{
+		// En cas d'erreur, on affiche un message et on arrête tout
+			die('Erreur : '.$e->getMessage());
+		}
+
+		// Si tout va bien, on peut continuer
+
+		// On récupère tout le contenu de la table jeux_video
+		$reponse = $bdd->query('SELECT nomkhlasse,annee from khlasse K,typekhlasse T where T.id_typekhlasse = K.id_type');
+
+		// On affiche chaque entrée une à une
+		while ($donnees = $reponse->fetch())
+		{
+		?>
+			<p>
+			<strong>Annee</strong> : <?php echo $donnees['annee']; ?><br />
+			
+			
+			<?php echo $donnees['nomkhlasse']; ?> nom de la khalsse </p>
+		<?php
+		}
+
+		$reponse->closeCursor(); 
+		?>
 		</div>
 	
 
