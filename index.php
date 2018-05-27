3548f24ae3c3435655e30d5cbcad766a02c0a811
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 19:55:59 +0200

    azezaeaze

--- index.php
+++ index.php
@@ -31,11 +31,13 @@
 		<div>
 		<h1> resultat</h1>
 			<?php
+			echo 'nimp';
 			require_once('controller/controller_khlasse.php');
 			$control = new khlasseController();
 			$data = $control -> getAllKhlasse();
 			$anne = $data[0]-> getAnnee();
 			echo '<h2> essai 123 </h2>' .$annee;
+			echo 'prout';
 			
 			?>
 
