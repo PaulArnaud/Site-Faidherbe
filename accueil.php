6d31cda36793eebe6c33dbaffb2c1919a7df46c2
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-27 21:55:13 +0200

    azeazeaze

--- accueil.php
+++ accueil.php
@@ -1,8 +1,12 @@
-<?php
+<html>
+<h1>ehehehhehe</h1>
+</html>
+
+<?php/*
 	try {
 		require_once("/controller/controller_accueil.php");
 	}
 	catch (Exception $e){
 		var_dump($e->getMessage());
-	}
+	}*/
 ?>
