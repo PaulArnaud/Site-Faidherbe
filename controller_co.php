f7fb0dccf24ff7546425ce888ce937b35d8b9e91
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 10:32:29 +0200

    azeaze

--- controller_co.php
+++ controller_co.php
@@ -3,17 +3,15 @@
 require_once("Users.php");
 $email=htmlspecialchars($_GET['Email']);
 $password=htmlspecialchars($_GET['password']);
-var_dump(Users::Check_Password($email,$password));
-echo '123';
-var_dump($cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20));
+$isGood = Users::Check_Password($email,$password));
 if(empty($email) || empty($password)){
   $message="Merci de remplir tous les champs!";
   //header("Location: Erreur.php?=".$message);
-}/*
+}
 else
 {
   //$password=sha1(sha1($password));
-  if(Users::Check_Password($email,$password)){
+  if($isGood){
     $cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
 		setcookie("cookieperso", $cookie, time()+(60*60*24*30), "/");
 		Users::Set_User_Cookie($email,$cookie);
@@ -23,5 +21,5 @@ else
     $message="Mauvais mdp";
     header("Location: Erreur.php?=".$message);
   }
-}*/
+}
 ?>
