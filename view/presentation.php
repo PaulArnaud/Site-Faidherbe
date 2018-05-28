c68b5ec84b96f600cb248214446214db82bf85b9
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 13:37:48 +0200

    azeaze

--- view/presentation.php
+++ view/presentation.php
@@ -1,72 +1,69 @@
-<!doctype html>
-<html lang="en">
-	<body>
-		<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-			<div class="col-md-10 p-lg-1 mx-auto my-5">
-				<h1 class="display-4 font-weight-normal">Bienvenue sur FaidherbeEnPoche</h1>
-				<p class="lead font-weight-normal">
-					Sur ce site vous êtes libres de faire ce que vous voulez dans la limite du respect. Il est conçu dans un objectif de réseau. Vous pourriez donc retrouvez vos anciens compagnons. Alors c'est parti, découvrez la 8ème merveille du monde.
-				</p>
-			</div>
+<body>
+	<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+		<div class="col-md-10 p-lg-1 mx-auto my-5">
+			<h1 class="display-4 font-weight-normal">Bienvenue sur FaidherbeEnPoche</h1>
+			<p class="lead font-weight-normal">
+				Sur ce site vous êtes libres de faire ce que vous voulez dans la limite du respect. Il est conçu dans un objectif de réseau. Vous pourriez donc retrouvez vos anciens compagnons. Alors c'est parti, découvrez la 8ème merveille du monde.
+			</p>
 		</div>
+	</div>
 
-		<div class=" bg-success mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-			<div class="my-3 py-3">
-				<h2 class="display-5"> La Gallerie </h2>
-				<p class="lead">
-					Retrouvez ici toutes les photos les plus belles de ces années folles.
-				</p>
-				<a class="btn btn-outline-light" href ng-click="tab = 5">Gallerie</a>
-			</div>
+	<div class=" bg-success mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+		<div class="my-3 py-3">
+			<h2 class="display-5"> La Gallerie </h2>
+			<p class="lead">
+				Retrouvez ici toutes les photos les plus belles de ces années folles.
+			</p>
+			<a class="btn btn-outline-light" href ng-click="tab = 5">Gallerie</a>
 		</div>
+	</div>
 
-		<div class=" position-relative bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Les Fonctions</h2>
-				<p class="lead">
-					Retrouvez ici les fonctions.
-				</p>
-				<a class="btn btn-outline-light" href ng-click="tab = 6">Fonctions</a>
-			</div>
+	<div class=" position-relative bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+		<div class="my-3 p-3">
+			<h2 class="display-5">Les Fonctions</h2>
+			<p class="lead">
+				Retrouvez ici les fonctions.
+			</p>
+			<a class="btn btn-outline-light" href ng-click="tab = 6">Fonctions</a>
 		</div>
+	</div>
 
-		<div class=" position-relative bg-danger mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Les Chants</h2>
-				<p class="lead">
-					Retrouvez tous les chants traditionnels de Faidherbe.
-				</p>
-				<a class="btn btn-outline-light" href ng-click="tab = 7">Chants</a>
-			</div>
+	<div class=" position-relative bg-danger mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+		<div class="my-3 p-3">
+			<h2 class="display-5">Les Chants</h2>
+			<p class="lead">
+				Retrouvez tous les chants traditionnels de Faidherbe.
+			</p>
+			<a class="btn btn-outline-light" href ng-click="tab = 7">Chants</a>
 		</div>
-		<!--
-		<div class=" position-relative bg-warning mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Les Evénements</h2>
-				<p class="lead">
-					Retrouvez tous les différents événements incontournables de Faidherbe.
-				</p>
-				<a class="btn btn-outline-light" href="../pageevenements/evenements.php">Evénements</a>
-			</div>
+	</div>
+	<!--
+	<div class=" position-relative bg-warning mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+		<div class="my-3 p-3">
+			<h2 class="display-5">Les Evénements</h2>
+			<p class="lead">
+				Retrouvez tous les différents événements incontournables de Faidherbe.
+			</p>
+			<a class="btn btn-outline-light" href="../pageevenements/evenements.php">Evénements</a>
 		</div>
-		-->
-		<div class=" position-relative bg-info mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Les Khâlots</h2>
-				<p class="lead">
-					Retrouvez tous les insignes et leur signification.
-				</p>
-				<a class="btn btn-outline-light" href ng-click="tab = 8">Khâlots</a>
-			</div>
+	</div>
+	-->
+	<div class=" position-relative bg-info mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+		<div class="my-3 p-3">
+			<h2 class="display-5">Les Khâlots</h2>
+			<p class="lead">
+				Retrouvez tous les insignes et leur signification.
+			</p>
+			<a class="btn btn-outline-light" href ng-click="tab = 8">Khâlots</a>
 		</div>
-		<div class="position-relative bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">La carte</h2>
-				<p class="lead">
-					Retrouvez ici une carte pour localiser les anciens.
-				</p>
-				<a class="btn btn-outline-light" href="https://drive.google.com/open?id=1ccwASpKw8JI7YLZ3VzykNfhJ9kawTSB-&usp=sharing">Carte</a>
-			</div>
+	</div>
+	<div class="position-relative bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+		<div class="my-3 p-3">
+			<h2 class="display-5">La carte</h2>
+			<p class="lead">
+				Retrouvez ici une carte pour localiser les anciens.
+			</p>
+			<a class="btn btn-outline-light" href="https://drive.google.com/open?id=1ccwASpKw8JI7YLZ3VzykNfhJ9kawTSB-&usp=sharing">Carte</a>
 		</div>
-	</body>
-</html>
+	</div>
+</body>
