c768bedc4c4d7efb5944d7523d30c738acf21b3f
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 21:47:31 +0200

    azeazeaze

--- controller_newkhlasse.php
+++ controller_newkhlasse.php
@@ -4,7 +4,13 @@ $typekhlasse=htmlspecialchars($_POST['type']);
 $annee=htmlspecialchars($_POST['annee']);
 echo $annee;
 echo $typekhlasse;
-var_dump(Users::Set_Khlasse($typekhlasse,$annee));
+require_once('connect.php');
+$bdheroku = myPDO();
+$req = $bdheroku->prepare('INSERT INTO khlasse(id_khlasse,id_type,annee)  VALUES ((SELECT count(*) FROM khlasse)+1,(SELECT id_typekhlasse FROM typekhlasse WHERE nomkhlasse =:typekh),:ann');
+var_dump($req);
+$req->bindParam(':typekh',$type);
+$req->bindParam(':ann',$year);
+$req->execute();
 //var_dump(Users::Set_Khlasse($typekhlasse,$annee));
 //header('Location: index.php');
 
