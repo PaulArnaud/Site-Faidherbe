6b14b7bedf1d00932c501e7223cf743888c63cce
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 12:42:44 +0200

    azeazea

--- view/accueil.php
+++ view/accueil.php
@@ -21,26 +21,17 @@
 					<li class="nav-item"><a class="nav-link"  href ng-click ="tab = 5">Administrateur</a></li>
 					<?php } ?>
 					<?php if(!isLogged()){ ?>
-					<li class="nav-item"><a class="nav-link" href="Connexion.php">Connexion</a></li>
+					<li class="nav-item"><a class="nav-link" href="Connexion.php"><i class="material-icons">power_setting</i></a></li>
 					<?php } ?>
 					<?php if(isLogged()){ ?>
 					<li class="nav-item">
-						<a class="nav-link"  title="Déconnexion" href="controller/Controller_Deconnexion.php"><i class="large material-icons">Deconnexion</i></a>
+						<a class="nav-link"  title="Déconnexion" href="controller/Controller_Deconnexion.php"><i class="material-icons">exit_to_app</i></a>
 					</li>
 					<?php } ?>
 				</ul>
 			</div>
 		</nav>
-		<div>
-		<?php 
-		echo 'prout';
-		var_dump(isLogged());
-		$user=Users::Get_User_Id($_COOKIE['cookieperso']);
-		var_dump($user);
-   		var_dump(!empty($user));
 
-		?>
-		</div>	
 		<div ng-show="tab === 1">
 			<?php require_once('view/presentation.php') ?>
 		</div>
