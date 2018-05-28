1ed74492ba3d41d0044de0e81bfc9184d48207ca
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 13:55:31 +0200

    azeaze

--- view/accueil.php
+++ view/accueil.php
@@ -11,17 +11,29 @@
 		<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top site-header" >
 			<div class="collapse navbar-collapse" id="navbarsExampleDefault" >
 				<ul class="navbar-nav mr-auto">
-					<li class="nav-item"><a class="nav-link" href ng-click ="tab = 1"><i class="material-icons">home</i></a></li>
+					<li class="nav-item">
+						<a class="nav-link" href ng-click ="tab = 1"><i class="material-icons">home</i></a>
+					</li>
 					<?php if(isLogged()){ ?>
-					<li class="nav-item"><a class="nav-link" href ng-click ="tab = 2"> <i class="material-icons">format_list_bulleted</i> </a></li>
-					<li class="nav-item"><a class="nav-link" href ng-click ="tab = 3"><i class="material-icons">person</i></a></li>
-					<li class="nav-item"><a class="nav-link" href ng-click ="tab = 4"> <i class="material-icons">chat</i> </a></li>
+					<li class="nav-item">
+						<a class="nav-link" href ng-click ="tab = 2"><i class="material-icons">format_list_bulleted</i></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href ng-click ="tab = 3"><i class="material-icons">person</i></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href ng-click ="tab = 4"><i class="material-icons">chat</i></a>
+					</li>
 					<?php } ?>
 					<?php if(isAdmin()){ ?>
-					<li class="nav-item"><a class="nav-link"  href ng-click ="tab = 5"><i class="material-icons">security</i></a></li>
+					<li class="nav-item">
+						<a class="nav-link"  href ng-click ="tab = 5"><i class="material-icons">security</i></a>
+					</li>
 					<?php } ?>
 					<?php if(!isLogged()){ ?>
-					<li class="nav-item"><a class="nav-link" href="Connexion.php"><i class="material-icons">power_settings_new</i></a></li>
+					<li class="nav-item">
+						<a class="nav-link" href="Connexion.php"><i class="material-icons">power_settings_new</i></a>
+					</li>
 					<?php } ?>
 					<?php if(isLogged()){ ?>
 					<li class="nav-item ">
@@ -31,7 +43,7 @@
 				</ul>
 			</div>
 		</nav>
-
+		
 		<div ng-show="tab === 1">
 			<?php require('view/presentation.php') ?>
 		</div>
@@ -56,10 +68,7 @@
 		<div ng-show="tab === 8">
 			<?php require('view/chapeau.php') ?>
 		</div>
-
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
 	</body>
 </html>
-
