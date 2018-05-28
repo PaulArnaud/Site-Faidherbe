8120fcc9db3d390343beda1a107b550a4f460a34
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-29 00:04:00 +0200

    zaezeea

--- controller_page_camarades.php
+++ controller_page_camarades.php
@@ -1,6 +1,6 @@
 <?php
 require_once("Users.php");
 //$khlasseID = htmlspecialchars($_GET['id']);
-$camarades = Users::Get_All_Camarades(0);
+$camarades = Users::Get_All_Camarades(1);
 require_once("view/camarades.php");
 ?>
