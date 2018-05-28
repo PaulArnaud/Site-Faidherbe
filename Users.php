44a6a5772fbb87d473d72f99f47f167d9ecaa357
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 11:06:11 +0200

    azeazea

--- Users.php
+++ Users.php
@@ -53,7 +53,7 @@ class Users
   public static function Get_Users_Role($userid){
     require_once('connect.php');
     $bdheroku = myPDO();
-    $req = $bdheroku->prepare("SELECT isAdmin FROM users WHERE id_user= :usersid");
+    $req = $bdheroku->prepare("SELECT users.isAdmin FROM users WHERE id_user= :usersid");
     $req->bindParam(':usersid',$userid);
     $req->execute();
     $data = $req->fetch();
