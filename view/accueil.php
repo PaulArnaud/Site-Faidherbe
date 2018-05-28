a8abd584655a4abe58fc14ad5671ebd6b03463b2
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 13:51:15 +0200

    azeazeaze

--- view/accueil.php
+++ view/accueil.php
@@ -54,8 +54,12 @@
 			<?php require('view/chants.php') ?>
 		</div>
 		<div ng-show="tab === 8">
-			<?php require('view/khalot.php') ?>
+			<?php require('view/chapeau.php') ?>
 		</div>
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
 	</body>
 </html>
 
--- view/communs/header.php
+++
@@ -1,3 +0,0 @@
-<meta charset="utf-8">
-<link rel="stylesheet" href="/lib/bootstrap.min.css">
-<script src="/lib/angular.min.js"></script>
