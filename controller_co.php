b443d36a1ed1be0ac2af2ce5ae9e7464610573fe
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 10:41:37 +0200

    aze

--- controller_co.php
+++ controller_co.php
@@ -3,6 +3,8 @@
 require_once("Users.php");
 $email=htmlspecialchars($_GET['Email']);
 $password=htmlspecialchars($_GET['password']);
+echo $email;
+echo $password;
 $isGood = Users::Check_Password($email,$password));
 var_dump($isGood);
 
