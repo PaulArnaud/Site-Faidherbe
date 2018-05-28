ddb20d9afbab15ca8d4428eeb76a07537fe09655
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 10:56:02 +0200

    azeazeazeaze

--- controller_co.php
+++ controller_co.php
@@ -14,6 +14,7 @@ if(Users::Check_Password($email,$password)){
   $cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
   var_dump($cookie);
   setcookie("cookieperso", $cookie, time()+(60*60*24*30), "/");
+  print_r($cookie);
   Users::Set_User_Cookie($email,$cookie);
   //header("Location: Accueil.php");
 }
