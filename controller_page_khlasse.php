5f59ab6c107bd70edcd2f5dddbdb59bff128bbf2
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 16:53:48 +0200

    zaeazeaze

--- controller_page_khlasse.php
+++ controller_page_khlasse.php
@@ -1,5 +1,10 @@
 <?php
-require("Khlasse.php");
-$listkhlasse = Khlasse::Get_All_Khlasse();
+try{
+    require_once("Khlasse.php");
+}
+catch (Exception $e){
+    echo 'erreur require';
+}
+//$listkhlasse = Khlasse::Get_All_Khlasse();
 require_once("view/khlasse.php");
 ?>
