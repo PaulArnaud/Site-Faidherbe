623cecbf19085398a65d911e1c4dbf87215d384a
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 20:47:25 +0200

    azeazeza

--- controller_newkhlasse.php
+++ controller_newkhlasse.php
@@ -1,6 +1,8 @@
 <?php
 require_once("Users.php");
-//$typekhlasse=htmlspecialchars($_POST['type']);
-//$annee=htmlspecialchars($_POST['annee']);
-//Users::Set_Khlasse($typekhlasse,$annee);
+$typekhlasse=htmlspecialchars($_POST['type']);
+$annee=htmlspecialchars($_POST['annee']);
+Users::Set_Khlasse($typekhlasse,$annee);
+header("Location: accueil.php");
+
 ?>
