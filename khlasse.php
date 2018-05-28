85ebda9736a4e24e8f1de15199191430ef09041a
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 17:07:29 +0200

    azeazeaze

--- khlasse.php
+++ khlasse.php
@@ -1,16 +1,3 @@
 <?php
-class Khlasse 
-{
-    public static function Get_All_Khlasse()
-    {
-        require_once('connect.php');
-        $bdheroku = myPDO();
-        $req = $bdheroku->prepare('SELECT * FROM  khlasse');
-        $req->execute();
-        while($data=$req->fetch()){
-            $result[] = $data;
-        }
-        return $result;
-    }
-}
+
 ?>
