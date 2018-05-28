b476f7fbdfd9773d209442b820049c4e61f8414a
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 19:17:57 +0200

    azeazeaz

--- view/khlasse.php
+++ view/khlasse.php
@@ -8,13 +8,13 @@
 					<thead class="thead-dark">
 						<tr>
 							<th class="text-center">
-								Année
+								                                Année                            
 							</th>
 							<th class="text-center">
-								Khlâsse
+								                                Khlâsse                            
 							</th>
 							<th class="text-center">
-								Nombre d'actifs
+								                                Nombre d'actifs                            
 							</th>
 						</tr>
 					</thead>
@@ -26,11 +26,28 @@
 				</div>
 			</div>
 		</main>
-
 		<footer class="footer">
 			<div class="container">
-				<a href ng-click="tab = " class="btn-dark btn-lg" role="button">Modifier mes infos</a>
+				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">  Launch demo modal</button>
 			</div>
 		</footer>
-</body>
-
+		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">
+							Modal title
+						</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					</div>
+					<div class="modal-body">
+						...
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="button" class="btn btn-primary">Save changes</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
