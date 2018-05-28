1dfe957d469468a23d31f15bea20566bc70b9e98
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 10:45:48 +0200

    azeazezea

--- controller_co.php
+++ controller_co.php
@@ -3,11 +3,25 @@
 require_once("Users.php");
 $email=htmlspecialchars($_GET['Email']);
 $password=htmlspecialchars($_GET['password']);
-echo $email;
-echo $password;
-
-var_dump(Users::Check_Password($email,$password)));
-
-
-
+var_dump(Users::Check_Password($email,$password));
+echo '123';
+var_dump($cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20));
+if(empty($email) || empty($password)){
+  $message="Merci de remplir tous les champs!";
+  //header("Location: Erreur.php?=".$message);
+}/*
+else
+{
+  //$password=sha1(sha1($password));
+  if(Users::Check_Password($email,$password)){
+    $cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
+		setcookie("cookieperso", $cookie, time()+(60*60*24*30), "/");
+		Users::Set_User_Cookie($email,$cookie);
+		header("Location: Accueil.php");
+  }
+  else {
+    $message="Mauvais mdp";
+    header("Location: Erreur.php?=".$message);
+  }
+}*/
 ?>
