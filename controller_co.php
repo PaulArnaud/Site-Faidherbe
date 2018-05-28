8b5588f098810d72efcafe65ca7be76fe3affa10
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 10:54:00 +0200

    azeazeaze

--- controller_co.php
+++ controller_co.php
@@ -3,14 +3,21 @@
 require_once("Users.php");
 $email=htmlspecialchars($_GET['Email']);
 $password=htmlspecialchars($_GET['password']);
-var_dump(Users::Check_Password($email,$password));
+//var_dump(Users::Check_Password($email,$password));
 echo '123';
-$cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
-var_dump($cookie);
 if(empty($email) || empty($password)){
   $message="Merci de remplir tous les champs!";
   //header("Location: Erreur.php?=".$message);
-}/*
+}
+if(Users::Check_Password($email,$password)){
+  echo '456';
+  $cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
+  var_dump($cookie);
+  setcookie("cookieperso", $cookie, time()+(60*60*24*30), "/");
+  Users::Set_User_Cookie($email,$cookie);
+  //header("Location: Accueil.php");
+}
+/*
 else
 {
   //$password=sha1(sha1($password));
