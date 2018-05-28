3e3713d00fc928d6c56df026553ee7e31273fc6d
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 20:12:33 +0200

    azeazeazeaz

--- view/createkhlasse.php
+++ view/createkhlasse.php
@@ -1,20 +1,22 @@
 <body>
-    <h1> <?php var_dump($types); ?> </h1>
+    <h1> <?php var_dump($types);
+    var_dump($types[0]);
+     ?> </h1>
 	<main>
 		<form action="controller_newkhlasse.php" method="post" >
 			<div class="row">
 				<div class="form-group col-md-4">
                     <div>
-                    <label for="inputState">State</label>
+                    <label for="inputState">Type de Khlasse</label>
                     <select id="inputState" class="form-control" name="type">
                         <option selected>Choose...</option>
-                        <?php var_dump($types);/*foreach ($types as $value){echo '<option>'.$value['nomkhlasse]'.'</option>';} */?></select>
+                        <?php /*foreach ($types as $value){echo '<option>'.$value['nomkhlasse]'.'</option>';} */?></select>
                     </div>
                     <div class="col">
                         <input type="text" class="form-control" name="annee">
                     </div>
                     <button class="btn btn-lg btn-dark" type="submit"> Enregistrer </button>
-                    </div>
+                </div>
 			</div>
 		</form>
 	</main>
