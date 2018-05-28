ced5d05622f796c8f169c2c14953d2754bdb7888
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 20:08:49 +0200

    azeaze

--- view/createkhlasse.php
+++ view/createkhlasse.php
@@ -1,8 +1,10 @@
 <body>
+    <h1> <?php var_dump($types); ?> </h1>
 	<main>
 		<form action="controller_newkhlasse.php" method="post" >
 			<div class="row">
 				<div class="form-group col-md-4">
+                    <div>
                     <label for="inputState">State</label>
                     <select id="inputState" class="form-control" name="type">
                         <option selected>Choose...</option>
@@ -12,6 +14,7 @@
                         <input type="text" class="form-control" name="annee">
                     </div>
                     <button class="btn btn-lg btn-dark" type="submit"> Enregistrer </button>
+                    </div>
 			</div>
 		</form>
 	</main>
