f657b98fbfe66be82979106dde984ad588abd55d
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-28 09:30:36 +0200

    azeazeaze

--- controller_co.php
+++ controller_co.php
@@ -11,6 +11,8 @@ if(empty($email) || empty($password)){
 else
 {
   $password=sha1(sha1($password));
+  echo $password;
+  echo Users::Check_Password($email,$password);
   if(Users::Check_Password($email,$password)){
     $cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
 		setcookie("cookieperso", $cookie, time()+(60*60*24*30), "/");
