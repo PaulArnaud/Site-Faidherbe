9d7842746fb911b2d813081fd7db1bf7df7970c9
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-29 17:02:56 +0200

    a

--- controller_modifinfo.php
+++ controller_modifinfo.php
@@ -5,18 +5,21 @@ $prenom = $_POST['prenom'];
 $portable = $_POST['portable'];
 $facebook = $_POST['facebook'];
 $linkedin = $_POST['linkedin'];
-//$kh1 = $_POST['khlasse1'];
-//$kh2 = $_POST['khlasse2'];
-//$ecole = $_POST['ecole'];
+$kh1 = $_POST['khlasse1'];
+$kh2 = $_POST['khlasse2'];
+$ecole = $_POST['ecole'];
 
-/*var_dump($nom);
+var_dump($nom);
 var_dump($prenom);
 var_dump($portable);
 var_dump($facebook);
-var_dump($linkedin);*/
-Users::Update_My_Data($nom,$prenom,$portable,$facebook,$linkedin);
+var_dump($linkedin);
+var_dump($kh1);
+var_dump($kh2);
+var_dump($ecole);
+//Users::Update_My_Data($nom,$prenom,$portable,$facebook,$linkedin);
 //Users::Insert_A_Etudie($kh1,$kh2)
 //Users::Insert_A_Etudie_PF($ecole);
-header('Location: accueil.php');
+//header('Location: accueil.php');
 ?>
 
