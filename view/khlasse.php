6a6673e840b495423f60455318cdd823f25278d0
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-29 11:05:25 +0200

    azeazeaez

--- view/khlasse.php
+++ view/khlasse.php
@@ -19,8 +19,8 @@
 						</tr>
 					</thead>
 					<tbody>
-						<tr action="controller_page_camarades.php" method="post" >
-							<?php foreach ($listkhlasse as $value) {echo '<tr id="'.$value['id_khlasse'].'">';echo '<td class="text-center">'.$value['annee'].'</td>';echo '<td class="text-center">'.$value['nomkhlasse'].'</td>';echo '<td class="text-center"> '.$_POST['idkhlasse']= $value['id_khlasse'].'<button class="btn btn-outline-light " type="submit" href ng-click="tab=11">'.$value[3].'</button>';echo '</tr>';}?></tr>
+						<tr>
+							<?php foreach ($listkhlasse as $value) {echo '<tr id="'.$value['id_khlasse'].'">';echo '<td class="text-center">'.$value['annee'].'</td>';echo '<td class="text-center">'.$value['nomkhlasse'].'</td>';echo '<td class="text-center"> <input type="hidden" name ="idkhlasse" value="'.$value['id_khlasse'].'"> <button action="controller_page_camarades.php" method="post" class="btn btn-outline-light " type="submit" href ng-click="tab=11">'.$value[3].'</button>';echo '</tr>';}?></tr>
 						</tbody>
 					</table>
 				</div>
