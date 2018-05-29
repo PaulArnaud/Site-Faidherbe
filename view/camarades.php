863e1bdb8accb82d13922bd79923125911209511
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-29 10:16:04 +0200

    azeaze

--- view/camarades.php
+++ view/camarades.php
@@ -5,9 +5,9 @@
 			<h1> Site du Faidherbard </h1>
 			<h2> Tableau des camarades de la Khlasse :<?php echo $khlasse['nomkhlasse'];?> de l'année : <?php echo $khlasse['annee'];?>  </h2>
 			<?php 
-			 $cookie=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
-			 $cookiegood = setcookie("cookiecamarades", $cookie, time()+(60*60*24*30), "/");
-			 var_dump($cookiegood)
+			$new=substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20);
+			$cookiegood = setcookie("cookiecamarades", $new, time()+(60*60*24*30), "/");
+			var_dump($cookiegood);
 			var_dump($_COOKIE["cookiecamarades"]); 
 			?>
 			<div class="table-responsive">
