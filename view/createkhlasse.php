cc26a553d5409ace4bfe8914bc2d31175d96ab7e
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-29 19:14:58 +0200

    aze

--- view/createkhlasse.php
+++ view/createkhlasse.php
@@ -1,7 +1,6 @@
 <body>
 	<main class="card text-center">
 		<form action="controller_newkhlasse.php" method="post" >
-		
 				<div class="form-group col-md-4">
                     <div class="col">
                     <label for="inputState">Créer une khlasse</label>
@@ -11,12 +10,11 @@
                     </div>
                     <div class="col">
                     <select id="inputState" class="form-control" name="annee">
-                            <option selected>Choisis un type de khlasse </option>
+                            <option selected>Choisis une année </option>
                             <?php foreach ($years as $value){echo '<option>'.$value['année'].'</option>';} ?></select>
                     </div>
                     <button class="btn btn-lg btn-dark" type="submit"> Enregistrer </button>
                 </div>
-			
 		</form>
 	</main>
 </body>
