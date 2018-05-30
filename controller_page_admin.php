208a2e3d727b7e591804458d6b7c7b439fd61adf
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-30 16:54:11 +0200

    aze

--- controller_page_admin.php
+++ controller_page_admin.php
@@ -3,6 +3,5 @@ require_once("Users.php");
 $users = Users::Get_All_Users();
 $ecoles = Users::Get_All_Ecole();
 $khlasses = Users::Get_All_Khlasse();
-require_once('controller/controller_admin.php');
 require_once('view/admin.php');
 ?>
