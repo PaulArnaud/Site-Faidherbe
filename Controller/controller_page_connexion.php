72086737b6d81b536df3eb184ab19c99b953aae7
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-30 21:02:34 +0200

    azeazeaz

--- Controller/controller_page_connexion.php
+++ Controller/controller_page_connexion.php
@@ -1,5 +1,5 @@
 <?php
-//require_once("Controller/controller_etat_user.php");
-//unloggedOnly();
-//require_once("view/connexion.php");
+require_once("controller_etat_user.php");
+unloggedOnly();
+require_once("../view/connexion.php");
 ?>
