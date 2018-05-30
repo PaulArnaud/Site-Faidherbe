dae60ae9c20f6a7ae766c4f4a77ffe8d386fd3b9
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-30 17:22:19 +0200

    azeaze

--- controller_page_admin.php
+++ controller_page_admin.php
@@ -1,5 +1,47 @@
 <?php
-require_once('models/test.php');
+//require_once('models/test.php');
+class test{
+    public static function Get_All_Users(){
+    require_once('/config/connect.php');
+    $bdheroku = myPDO();
+    $req = $bdheroku->prepare('SELECT * 
+    FROM users');
+    $req->execute();
+    while($data=$req->fetch())
+		{
+			$result[] = $data;
+		}
+	return $result;
+  }
+
+  public static function Get_All_Khlasse(){
+      require_once('/config/connect.php');
+      $bdheroku = myPDO();
+      $req = $bdheroku->prepare('SELECT E.id_khlasse,T.nomkhlasse,annee,(SELECT count(*) 
+      FROM a_etudie A 
+      WHERE A.id_khlasse= E.id_khlasse) 
+      FROM  khlasse E,typekhlasse T 
+      WHERE T.id_typekhlasse = E.id_type');
+      $req->execute();
+      while($data=$req->fetch()){
+          $result[] = $data;
+      }
+      return $result;
+  }
+
+  public static function Get_All_Ecole(){
+      require_once('/config/connect.php');
+      $bdheroku = myPDO();
+      $req = $bdheroku->prepare('SELECT id_ecole,nomecole 
+      FROM ecole');
+      $req->execute();
+      while($data=$req->fetch()){
+          $result[] = $data;
+      }
+      return $result;
+  }
+}
+
 $users = test::Get_All_Users();
 var_dump($users);
 $ecoles = test::Get_All_Ecole();
