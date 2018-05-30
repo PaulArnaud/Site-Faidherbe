4ea71a817f8782bd4a4501c0463f12ae9758c3f9
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-30 12:30:31 +0200

    azeaze

--- controller_page_individu.php
+++ controller_page_individu.php
@@ -6,5 +6,5 @@ $infouser = Users::Get_Info_User($userID);
 $ecoleuser = Users::Get_School_User($userID);
 $GLOBALS['individu'] = true;
 echo("<div ng-init='tab=12' > </div>");
-require_once('controller_accueil.php');
+require_once('accueil.php');
 ?>
