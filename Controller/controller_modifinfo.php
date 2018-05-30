48539a478819dd3d95cb568a9f0101781b02da86
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-30 20:52:46 +0200

    aze

--- Controller/controller_modifinfo.php
+++ Controller/controller_modifinfo.php
@@ -16,6 +16,6 @@ Users::Update_My_Data($nom,$prenom,$portable,$facebook,$linkedin);
 Users::Insert_A_Etudie($id_khlasse1[0]);
 Users::Insert_A_Etudie($id_khlasse2[0]);
 Users::Insert_A_Etudie_PF($id_ecole[0]);
-header('Location: Controller/controller_page_accueil.php');
+header('Location: controller_page_accueil.php');
 ?>
 
