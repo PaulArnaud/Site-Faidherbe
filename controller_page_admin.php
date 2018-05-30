51ac6eb71bd642ffbac212a96799325caa7120d1
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-30 17:19:40 +0200

    azeazeaze

--- controller_page_admin.php
+++ controller_page_admin.php
@@ -1,6 +1,7 @@
 <?php
 require_once('models/test.php');
 $users = test::Get_All_Users();
+var_dump($users);
 $ecoles = test::Get_All_Ecole();
 $khlasses = test::Get_All_Khlasse();
 require_once('view/admin.php');
