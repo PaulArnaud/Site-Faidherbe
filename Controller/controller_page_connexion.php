1caa0429e02076bac79c8f5b099ee445893754af
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-30 21:09:20 +0200

    er

--- Controller/controller_page_connexion.php
+++ Controller/controller_page_connexion.php
@@ -1,7 +1,8 @@
 <?php
-echo (dirname(__FILE__));
-/*
-require_once("controller_etat_user.php");
+
+require_once("Controller/controller_etat_user.php");/*
 unloggedOnly();
 require_once("../view/connexion.php");*/
+
+
 ?>
