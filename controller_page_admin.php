32521283b002e2c7b0179f7d795d1e9a5eb88554
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-30 17:07:03 +0200

    aze

--- controller_page_admin.php
+++ controller_page_admin.php
@@ -1,6 +1,6 @@
 <?php
 echo'123';
-var_dump(models/Users.php);
+var_dump('models/Users.php');
 //require_once('/app/models/Users.php');
 /*$users = Users::Get_All_Users();
 $ecoles = Users::Get_All_Ecole();
