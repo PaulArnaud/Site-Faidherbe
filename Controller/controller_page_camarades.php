da658f55cf2cf7c3493b704db94a4d942413b5e2
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 20:41:32 +0200

    rrrrr

--- Controller/controller_page_camarades.php
+++ Controller/controller_page_camarades.php
@@ -4,6 +4,7 @@ $khlasseID = $_GET['idkhlasse'];
 $var1 = Khlasse::Get_Khlasse($khlasseID);
 $var2 = Khlasse::Get_All_Camarades($khlasseID);
 $GLOBALS['camarades'] = true;
-echo("<div ng-init='tab=11' > allo </div>");
-//header('Location: ../accueil.php');
+echo("<div ng-init='tab=11' ></div>");
+header('Location: ../accueil.php');
+exit();
 ?>
