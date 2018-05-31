542c265294211f2daf51a3a9065aa0a07424b311
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 18:47:55 +0200

    azeaze

--- view/connexion.php
+++ view/connexion.php
@@ -4,7 +4,7 @@
 		<title> Ceci est la page de connexion </title>
 	</head>
 	<body class="text-center">
-		<form class="form-signin container" action="controller_connexion.php" method="post">
+		<form class="form-signin container" action="Controller/controller_connexion.php" method="post">
 			<img class="mb-4" src="/img/logo faidherbe.jpg" alt="" width="150" height="150">
 			<h1 class="h3 mb-3 font-weight-normal"> Bienvenue </h1>
 
