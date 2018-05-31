61f8a96e3d390c470522eea40ef3428517912e43
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 22:05:40 +0200

    rrrrr

--- Controller/controller_page_individu.php
+++ Controller/controller_page_individu.php
@@ -1,12 +1,9 @@
 <?php
-require_once("Users.php");
+require_once("../Users.php");
 $userID = $_GET['iduser'];
 $khlasseuser = Users::Get_Khlasse_User($userID);
 $infouser = Users::Get_Info_User($userID);
 $ecoleuser = Users::Get_School_User($userID);
-$GLOBALS['individu'] = true;
-$GLOBALS['action'] = 2;
 
-header('Location: ../accueil.php');
-exit();
+require('../view/individu.php');
 ?>
