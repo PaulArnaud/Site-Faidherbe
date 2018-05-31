b9c7b8968420e52cc03fc3015add178f76e17990
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 19:09:27 +0200

    azeaze

--- view/connexion.php
+++ view/connexion.php
@@ -1,4 +1,5 @@
-<link href="css/signin.css" rel="stylesheet">
+<body class="text-center">
+	<link href="css/signin.css" rel="stylesheet">
 	<form class="form-signin container" action="Controller/controller_connexion.php" method="post">
 		<img class="mb-4" src="/img/logo faidherbe.jpg" alt="" width="150" height="150">
 		<h1 class="h3 mb-3 font-weight-normal"> Bienvenue </h1>
@@ -17,3 +18,4 @@
 			&copy; Faidherbard
 		</p>
 	</form>
+</body>
