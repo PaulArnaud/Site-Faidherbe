f4f4f8ab35894aa98391823e2e155723c3681285
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 17:57:51 +0200

    azeaze

--- view/accueil.php
+++ view/accueil.php
@@ -74,7 +74,15 @@
 		</div>
 
 		<div ng-show="tab === 9">
-			<?php require_once('Controller/controller_page_infoperso.php') ?>
+			<?php 
+			require_once("Users.php");
+			require_once("Ecole.php");
+			require_once("Khlasse.php");
+			$profil = Users::Get_Info();
+			$khlasses = Khlasse::Get_All_Khlasse();
+			$ecoles = Ecole::Get_All_Ecole();
+			require_once("view/Infoperso.php"); 
+			?>
 		</div>
 		<div ng-show="tab === 10">
 			<?php require_once('Controller/controller_page_newkhlasse.php') ?>
