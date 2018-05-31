5db21cd6be5c61ccaf7f7c17d98969e27d438577
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 20:15:51 +0200

    aze

--- view/khlasse.php
+++ view/khlasse.php
@@ -1,8 +1,26 @@
+
+<script type="text/javascript">
+public static function Store_Id_Khlasse($value){
+    $_POST['stored_id_khlasse'] = $value;
+}
+</script>
+
 <body>
 	<main role="main" class="container text-center">
 		<div class="starter-template">
 			<h1> Site du Faidherbard </h1>
 			<h2> Tableau des Khlâsses </h2>
+
+			<div class="btn-group btn-group-toggle" data-toggle="buttons">
+				<div class="container">
+					<a href ng-click="tab = 10 " class="btn-dark btn-lg" role="button"> Ajouter une khlasse </a>
+				</div>
+				<div class="container">
+					<a href ng-click="tab = 13 " class="btn-dark btn-lg" role="button"> Ajouter une école </a>
+				</div>
+			</div>
+
+
 			<div class="table-responsive">
 				<table class="table table-striped table-sm table-bordered table-dark table-hover">
 					<thead class="thead-dark">
@@ -20,21 +38,30 @@
 					</thead>
 					<tbody>
 						<tr>
-							<?php foreach ($listkhlasse as $value) {echo '<tr id="'.$value['id_khlasse'].'">';echo '<td class="text-center">'.$value['annee'].'</td>';echo '<td class="text-center">'.$value['nomkhlasse'].'</td>';echo '<td class="text-center"> <a class="btn btn-outline-light" ng-click="tab=0" href="Controller/controller_page_camarades.php?idkhlasse='.$value['id_khlasse'].'">'.$value[3].'</a>';echo '</tr>';}?></tr>
+							<?php foreach ($listkhlasse as $value) {echo '<tr id="'.$value['id_khlasse'].'">';echo '<td class="text-center">'.$value['annee'].'</td>';echo '<td class="text-center">'.$value['nomkhlasse'].'</td>';echo '<td class="text-center"> <a class="btn btn-outline-light" onclick="Store_Id_Khlasse('.$value['id_khlasse'].')">'.$value[3].'</a>';echo '</tr>';}?></tr>
 						</tbody>
 					</table>
 				</div>
+
+				<?php 
+				echo $_POST['stored_id_khlasse'];
+				?>
+
+
+
+
+
+
+
+
+
+
+
+
 			</div>
 		</main>
 
-		<div class="btn-group btn-group-toggle" data-toggle="buttons">
-			<div class="container">
-				<a href ng-click="tab = 10 " class="btn-dark btn-lg" role="button"> Ajouter une khlasse </a>
-			</div>
-			<div class="container">
-				<a href ng-click="tab = 13 " class="btn-dark btn-lg" role="button"> Ajouter une école </a>
-			</div>
-		</div>
+
 </body>
 
 
