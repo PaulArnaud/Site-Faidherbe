cad895e016e2e5772dafa0db4bdab9467be02447
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 20:19:04 +0200

    azeazeaze

--- view/khlasse.php
+++ view/khlasse.php
@@ -37,7 +37,7 @@ function Store_Id_Khlasse($value){
 					</thead>
 					<tbody>
 						<tr>
-							<?php foreach ($listkhlasse as $value) {echo '<tr id="'.$value['id_khlasse'].'">';echo '<td class="text-center">'.$value['annee'].'</td>';echo '<td class="text-center">'.$value['nomkhlasse'].'</td>';echo '<td class="text-center"> <a class="btn btn-outline-light" onclick="Store_Id_Khlasse('.$value['id_khlasse'].')">'.$value[3].'</a>';echo '</tr>';}?></tr>
+							<?php foreach ($listkhlasse as $value) {echo '<tr id="'.$value['id_khlasse'].'">';echo '<td class="text-center">'.$value['annee'].'</td>';echo '<td class="text-center">'.$value['nomkhlasse'].'</td>';echo '<td class="text-center"> <button class="btn btn-outline-light" onclick="Store_Id_Khlasse('.$value['id_khlasse'].')" > '.$value[3].' </button>';echo '</tr>';}?></tr>
 						</tbody>
 					</table>
 				</div>
