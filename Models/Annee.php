2ee23ed9bdd9d6eff2ecb3ffd16c7952ff3933ec
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 10:03:02 +0200

    aze

--- Models/Annee.php
+++ Models/Annee.php
@@ -1,7 +1,7 @@
 <?php
 class Annee{
     public static function Get_Years(){
-        require_once('config/connect.php');
+        require_once('/config/connect.php');
         $bdheroku = myPDO();
         $req = $bdheroku->prepare('SELECT année FROM année');
         $req->execute();
