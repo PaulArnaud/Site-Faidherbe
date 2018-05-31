27d8705775eb4d0c11856b3e622a1fa9b627b8d1
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 21:08:33 +0200

    aze

--- Controller/controller_page_camarades.php
+++ Controller/controller_page_camarades.php
@@ -5,8 +5,8 @@ $var1 = Khlasse::Get_Khlasse($khlasseID);
 $var2 = Khlasse::Get_All_Camarades($khlasseID);
 $GLOBALS['camarades'] = true;
 $GLOBALS['action'] = 1;
-var_dump($GLOBALS['action']);
-/*
+
+
 header('Location: ../accueil.php');
-exit();*/
+exit();
 ?>
