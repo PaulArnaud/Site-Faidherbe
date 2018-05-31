4bf461ba62e15e730935a982189ef1c7efffe622
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-05-31 19:11:38 +0200

    aze
    aze

--- view/connexion.php
+++ view/connexion.php
@@ -1,5 +1,5 @@
 <body class="text-center">
-	<link href="css/signin.css" rel="stylesheet">
+	<!-- <link href="css/signin.css" rel="stylesheet">-->
 	<form class="form-signin container" action="Controller/controller_connexion.php" method="post">
 		<img class="mb-4" src="/img/logo faidherbe.jpg" alt="" width="150" height="150">
 		<h1 class="h3 mb-3 font-weight-normal"> Bienvenue </h1>
