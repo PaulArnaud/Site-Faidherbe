b62427a563a7d46576e7d9456dbeb6d9e776f33a
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-06-02 12:25:59 +0200

    aze

--- accueil.php
+++ accueil.php
@@ -4,8 +4,9 @@
 		<title>Accueil FaidherbeEnPoche </title>
 		<link rel="stylesheet" href="lib/bootstrap.min.css">
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 		<script src="lib/angular.min.js"></script>
 	</head>
 	<body ng-init="tab = 1">
