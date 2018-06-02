982063226908ae684cd8894f846b4936b1736a26
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-06-02 10:57:58 +0200

    azeaze

--- Annee.php
+++ Annee.php
@@ -1,20 +1,17 @@
 <?php
 
-
-$db = parse_url(getenv("DATABASE_URL"));
-
-$pdo = new PDO("pgsql:" . sprintf(
-  "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-  $db["host"],
-  $db["port"],
-  $db["user"],
-  $db["pass"],
-  ltrim($db["path"], "/")
-));
-
-
 class Annee{
     public static function Get_Years(){
+      $db = parse_url(getenv("DATABASE_URL"));
+
+      $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+        ));
         $req = $pdo->prepare('SELECT année FROM année');
         $req->execute();
         while($data=$req->fetch()){
