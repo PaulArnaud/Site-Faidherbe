eff44434ff2dc541489c8bb541044d08018628b1
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-06-02 11:43:15 +0200

    azr

--- Controller/controller_admin.php
+++ Controller/controller_admin.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../Models/Users.php");
-require_once(".../Models/Ecole.php");
-require_once(".../Models/Khlasse.php");
+//require_once(".../Models/Ecole.php");
+//require_once(".../Models/Khlasse.php");
 /*
 $user = $_POST['user'];
 $ecole = $_POST['ecole'];
