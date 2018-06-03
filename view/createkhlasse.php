bbc75201ccdb31b4a7804489a7d7364743122059
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-06-03 09:33:49 +0200

    zaezae

--- view/createkhlasse.php
+++ view/createkhlasse.php
@@ -1,20 +1,17 @@
-<body class="container">
-	<main class="card text-center">
-		<form action="Controller/controller_newkhlasse.php" method="post" >
-				<div class="form-group col-md-4">
-                    <div class="col">
-                    <label for="inputState">Créer une khlasse</label>
-                        <select id="inputState" class="form-control" name="type">
-                            <option selected>Choisis un type de khlasse </option>
-                            <?php foreach ($types as $value){echo '<option>'.$value['nomkhlasse'].'</option>';} ?></select>
-                    </div>
-                    <div class="col">
-                    <select id="inputState" class="form-control" name="annee">
-                            <option selected>Choisis une année </option>
-                            <?php foreach ($years as $value){echo '<option>'.$value['année'].'</option>';} ?></select>
-                    </div>
-                    <button class="btn btn-lg btn-dark" type="submit"> Enregistrer </button>
-                </div>
-		</form>
-	</main>
-</body>
+
+<form class = "container card vertical-align align-items-center" action="Controller/controller_newkhlasse.php" method="post" >
+                <div class="form-group col-md-4">
+        <div class="col">
+        <label for="inputState">Créer une khlasse</label>
+        <select id="inputState" class="form-control" name="type">
+                <option selected>Choisis un type de khlasse </option>
+                <?php foreach ($types as $value){echo '<option>'.$value['nomkhlasse'].'</option>';} ?></select>
+        </div>
+        <div class="col">
+        <select id="inputState" class="form-control" name="annee">
+                <option selected>Choisis une année </option>
+                <?php foreach ($years as $value){echo '<option>'.$value['année'].'</option>';} ?></select>
+        </div>
+        <button class="btn btn-lg btn-dark" type="submit"> Enregistrer </button>
+</div>
+</form>
