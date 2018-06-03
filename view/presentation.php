1e1542688df056d70730ee91191f4393773e0362
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-06-03 09:26:05 +0200

    azrarz

--- view/presentation.php
+++ view/presentation.php
@@ -8,7 +8,7 @@
 		</div>
 	</div>
 
-	<div class=" position-relative bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+	<div class=" position-relative bg-info mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
 		<div class="my-3 p-3">
 			<h2 class="display-5">Les Fonctions</h2>
 			<p class="lead">
@@ -18,13 +18,13 @@
 		</div>
 	</div>
 
-	<div class=" position-relative bg-danger mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+	<div class=" position-relative bg-info mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
 		<div class="my-3 p-3">
 			<h2 class="display-5">Les Chants</h2>
 			<p class="lead">
 				Retrouvez tous les chants traditionnels de Faidherbe.
 			</p>
-			<a class="btn btn-outline-light" href ng-click="tab = 7">Chants</a>
+			<a class="btn btn-outline-light bg-danger" href ng-click="tab = 7">Chants</a>
 		</div>
 	</div>
 
@@ -34,16 +34,16 @@
 			<p class="lead">
 				Retrouvez tous les insignes et leur signification.
 			</p>
-			<a class="btn btn-outline-light" href ng-click="tab = 8">Khâlots</a>
+			<a class="btn btn-outline-light bg-danger" href ng-click="tab = 8">Khâlots</a>
 		</div>
 	</div>
-	<div class="position-relative bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+	<div class="position-relative bg-info mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
 		<div class="my-3 p-3">
 			<h2 class="display-5">La carte</h2>
 			<p class="lead">
 				Retrouvez ici une carte pour localiser les anciens.
 			</p>
-			<a class="btn btn-outline-light" href="https://drive.google.com/open?id=1ccwASpKw8JI7YLZ3VzykNfhJ9kawTSB-&usp=sharing">Carte</a>
+			<a class="btn btn-outline-light bg-danger" href="https://drive.google.com/open?id=1ccwASpKw8JI7YLZ3VzykNfhJ9kawTSB-&usp=sharing">Carte</a>
 		</div>
 	</div>
 </body>
