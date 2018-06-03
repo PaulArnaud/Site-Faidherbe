11249676cb07f298f4a4c6e71a6f089a42ef1a00
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-06-03 10:00:35 +0200

    arz

--- view/individu.php
+++ view/individu.php
@@ -77,7 +77,7 @@
         </div>
     </main>
     <div class="container">
-		<a href="../accueil.php" class="btn-danger btn-lg" role="button"> Retour à l'accueil </a>
+		<a href="../accueil.php" class="btn-danger btn-lg align-items-center" role="button"> Retour à l'accueil </a>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
