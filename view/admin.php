b7934dfb9106f57d23570723c11a820acbf3bb1e
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-06-03 10:05:55 +0200

    azeazea

--- view/admin.php
+++ view/admin.php
@@ -1,7 +1,10 @@
 
-
+<h3 class="text-cetner"> Page du pouvoir </h3>
 
 <form class="form-signin container card vertical-align align-items-center"  action="Controller/controller_admin.php" method="post">
+	
+	<label for="inputState"> Suppression : </label>
+	
 	<div class="container">
 		<div class="input-group mb-3">
 			<select class="custom-select"  name="user" >
